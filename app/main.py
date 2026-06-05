1ba3a34ad371a40a282191eeb72ab85b2c5eeaef
Neokitoss <kpokodiji13@gmail.com>
2026-06-06 00:47:59 +0300

    start of developing py-create-file

--- app/main.py
+++ app/main.py
@@ -1,9 +1,9 @@
 from datetime import datetime  # DO NOT CHANGE THIS IMPORT
 
 
-def main():
-    # write your code here
-    pass
+def main() -> None:
+    while True:
+        now = datetime.now()
 
 
 if __name__ == "__main__":
